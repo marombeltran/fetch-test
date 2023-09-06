2bc25d6ef34108f3c4fa0d8ac92a12de74154d27
Marom Beltran <mrmbeltran@proton.me>
2023-09-06 17:18:43 -0600

    Change - list generate

--- 5-generate.js
+++ 5-generate.js
@@ -1,14 +1,15 @@
 
-const jsonStream= 'https://jsonplaceholder.typicode.com/users'
+const jsonStream= 'https://random-data-api.com/api/v2/users?size=10'
 const imgStream= 'https://picsum.photos/id/237/300/200'
 const fontStream= 'https://fonts.gstatic.com/s/montserrat/v25/JTUHjIg1_i6t8kCHKm4532VJOt5-QNFgpCs16Hw5aXp-p7K4KLg.woff2'
 const htmlStream= 'https://example.com'
 
 export const getData = () => {
-    const li = document.createElement('li')
+    const ul = document.createElement('ul')
     const img = document.createElement('img')
     const header = document.createElement('header')
 
+    // ...
     fetch(imgStream).then( response => {
         if (! response.ok) {
             throw new Error('was not a valid request')
@@ -18,10 +19,22 @@ export const getData = () => {
     }).then( blob => {
         const img = document.createElement('img')
         img.src = URL.createObjectURL(blob)
-        console.log(img.src)
-
         document.body.append(img)
     }).catch( error => {
         console.warn(error.message)
     })
+
+    // ...
+    fetch(jsonStream).then( response => {
+        return response.json()
+    }).then( jsonObject => {
+        ul.innerHTML = jsonObject.map( ({ uid, first_name, last_name }) => {
+            return `<li data-uid="${uid}">
+                <p>${first_name} ${last_name}</p>
+            </li>`
+        }).join('')
+
+        document.body.append(ul)
+    })
+
 }
