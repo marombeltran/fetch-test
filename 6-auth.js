de12817a69bc14de9f5063e71812a89958b219e3
Marom Beltran <mrmbeltran@proton.me>
2023-09-07 20:43:41 -0600

    cors

--- 6-auth.js
+++ 6-auth.js
@@ -8,7 +8,14 @@ export const getData = async () => {
     params.append('age', '35')
 
     const request = new Request(url, {
-
+        headers: {
+            // 'origin': 'https://cia.org', // this is not allowed because the key corresponds to the web browser
+            // Forbidden header names: https://developer.mozilla.org/en-US/docs/Glossary/Forbidden_header_name
+            'x-api-key': 'fasdfasdfasdf', // API key
+            'Authorization': 'Bearer akjfaksjfakdfklajsdfkj', // JWToken
+        },
+        cache: "default",
+        credentials: "same-origin", // the opposite "include"
     });
 
     const jsonObject = await (await fetch(request)).json()
